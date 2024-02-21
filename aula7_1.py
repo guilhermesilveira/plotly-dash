750d10214c2fc336d7e6bac611790368917b49df
Guilherme Silveira <guilherme.silveira@alura.com.br>
2024-02-21 09:13:33 -0300

    gcloud

--- aula7_1.py
+++ aula7_1.py
@@ -10,9 +10,16 @@ gcloud init
 
 # https://console.developers.google.com
 
+# cloudignore deve adicionar o .venv
+
 gcloud app deploy
 # app.yaml
 # 16 south america
 # billing
 
 # target URI
+
+# cloud engine: inicializa uma maquina (virtual) e instala o pip install e roda gunicorn isso ai
+# se passar X horas (minutos) e ninguem acessar, ele derruba
+# quando acessar novamente, ele sobe novamente
+# entao, se nao tem acesso frequente, fica lerdo o primeiro acesso
